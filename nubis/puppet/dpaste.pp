a266703fc5a8815c36d82c357b5d00ae07f61bc8
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-02-12 10:52:14 -0500

    Fix requrements.txt since 2.0 is out and not good for us

--- nubis/puppet/dpaste.pp
+++ nubis/puppet/dpaste.pp
@@ -17,11 +17,18 @@ file { '/usr/var':
   ensure => directory,
 }
 
+
+# Fix requrements.txt since 2.0 is out and not good for us
+python::pip { 'django':
+  ensure => '<2.0,>=1.11',
+}
+
 # pip install requirements
 python::requirements { '/var/www/dpaste/requirements.txt':
   require => [
     Class['python'],
     Class['mysql::bindings'],
+    Python::Pip['django'],
   ]
 }
 
