5eb038192fa352d1a25a71f0ed3924c7e2f4040b
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-02-12 10:33:11 -0500

    Upgrade to Nubis v2.1.0

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -1,5 +1,5 @@
 module "worker" {
-  source       = "github.com/nubisproject/nubis-terraform//worker?ref=v2.0.1"
+  source       = "github.com/nubisproject/nubis-terraform//worker?ref=v2.1.0"
   region       = "${var.region}"
   environment  = "${var.environment}"
   account      = "${var.account}"
@@ -25,7 +25,7 @@ module "worker" {
 }
 
 module "load_balancer" {
-  source       = "github.com/nubisproject/nubis-terraform//load_balancer?ref=v2.0.1"
+  source       = "github.com/nubisproject/nubis-terraform//load_balancer?ref=v2.1.0"
   region       = "${var.region}"
   environment  = "${var.environment}"
   account      = "${var.account}"
@@ -34,7 +34,7 @@ module "load_balancer" {
 }
 
 module "database" {
-  source                 = "github.com/nubisproject/nubis-terraform//database?ref=v2.0.1"
+  source                 = "github.com/nubisproject/nubis-terraform//database?ref=v2.1.0"
   region                 = "${var.region}"
   environment            = "${var.environment}"
   account                = "${var.account}"
@@ -43,7 +43,7 @@ module "database" {
 }
 
 module "dns" {
-  source       = "github.com/nubisproject/nubis-terraform//dns?ref=v2.0.1"
+  source       = "github.com/nubisproject/nubis-terraform//dns?ref=v2.1.0"
   region       = "${var.region}"
   environment  = "${var.environment}"
   account      = "${var.account}"
